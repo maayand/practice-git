c2af8f51da0f63705533a997ee9c64265ddd92e2
Maayan Weiss Daester <mayanmarta@gmail.com>
2026-05-06 12:24:12 +0300

    Limit ball size to 400px

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ function onBallClick(el){
     
     var newWidth = currentWidth + 50
     var newHeight  = currentHeight + 50
-
+    if(newWidth >= 400){
+        newHeight = 100
+        newWidth = 100
+    }
     el.style.width = newWidth + 'px'
     el.style.height = newHeight + 'px'
 
