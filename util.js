a940bd96918a573c41a53592d1b95ba7dbd1c06c
Maayan Weiss Daester <mayanmarta@gmail.com>
2026-05-10 14:00:03 +0300

    Add getrandomcolor function to utils

--- util.js
+++ util.js
@@ -5,3 +5,12 @@ function getRandomInt(min, max){
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min ) + min)
 }
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
