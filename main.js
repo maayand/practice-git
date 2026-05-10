9dd2f71d4316c591e050136b9f9b0a223aa5d97a
Maayan Weiss Daester <mayanmarta@gmail.com>
2026-05-10 13:52:04 +0300

    Make ball growth random between 20px and 60px

--- main.js
+++ main.js
@@ -2,17 +2,19 @@
 
 function onBallClick(el){
     // console.log( el.style )
-    var currentWidth = el.offsetWidth
-    var currentHeight = el.offsetHeight
+    // var currentWidth = el.offsetWidth
+    // var currentHeight = el.offsetHeight
     
-    var newWidth = currentWidth + 50
-    var newHeight  = currentHeight + 50
-    if(newWidth >= 400){
-        newHeight = 100
-        newWidth = 100
-    }
-    el.style.width = newWidth + 'px'
-    el.style.height = newHeight + 'px'
+    // var newWidth = currentWidth + 50
+    // var newHeight  = currentHeight + 50
+    // if(newWidth >= 400){
+    //     newHeight = 100
+    //     newWidth = 100
+    // }
+    var currentWidth = getRandomInt(20, 60)
+   
+    el.style.width = currentWidth + 'px'
+    el.style.height = currentWidth + 'px'
 
-    el.innerText = newWidth;
+    el.innerText = currentWidth;
 }
