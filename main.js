a97469b16da9620a373073e0633aacf050b2cf79
Maayan Weiss Daester <mayanmarta@gmail.com>
2026-05-10 14:02:11 +0300

    Add random color to onballclick function

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 
 function onBallClick(el){
     var randomDiff = getRandomInt(20, 60);
+    var randomColor = getRandomColor()
     var currentWidth = el.offsetWidth;
     var newWidth = currentWidth + randomDiff;
     if (newWidth > 400) {
         newWidth = 100;
     }
+    el.style.background = randomColor;
     el.style.width = newWidth + 'px';
     el.style.height = newWidth + 'px';
     el.innerText = newWidth;
